a45c3293ed586ff31531cb4dc2d71780f23a47a0
Melvin K Varghese <mevivrg01@gmail.com>
2024-04-08 15:06:30 +0100

    moded client app

--- legal_advice_project/client/tests.py
+++ legal_advice_project/client/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Client
 
-# Create your tests here.
+class ClientTestCase(TestCase):
+    def setUp(self):
+        Client.objects.create(name='John Doe', email='john@example.com')
+
+    def test_client_created(self):
+        client = Client.objects.get(name='John Doe')
+        self.assertEqual(client.email, 'john@example.com')
