eb7be7a2de2ae6518a0fa993f4e6f85f82fcb4a7
Melvin K Varghese <mevivrg01@gmail.com>
2024-04-08 14:36:28 +0100

    apps added, modified settings and url files

--- legal_advice_project/legal_advice_project/urls.py
+++ legal_advice_project/legal_advice_project/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Serve static files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serve media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
